fa2745f53db16414f9e20b4945e9816e16565ccd
Brian Yeh <brian.yeh@coolbitx.com>
2021-10-30 17:00:59 +0800

    test: complete to setup test fixture

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,10 +1,11 @@
 // migrating the appropriate contracts
 var ERC721MintableComplete = artifacts.require('ERC721MintableComplete');
 var SquareVerifier = artifacts.require('SquareVerifier');
-// TODO: var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+var SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
 
 module.exports = function (deployer) {
   deployer.deploy(ERC721MintableComplete);
-  deployer.deploy(SquareVerifier);
-  // TODO: deployer.deploy(SolnSquareVerifier);
+  deployer.deploy(SquareVerifier).then(() => {
+    deployer.deploy(SolnSquareVerifier, SquareVerifier.address);
+  });
 };
--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,19 @@
 // Test if a new solution can be added for contract - SolnSquareVerifier
-
 // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+const SquareVerifier = artifacts.require('SquareVerifier');
+const SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
+const proof = require('../../zokrates/code/square/proof.json');
+
+contract('SolnSquareVerifer', (accounts) => {
+  let verifier;
+  let soln;
+  beforeEach(async () => {
+    verifier = await SquareVerifier.new({ from: accounts[0] });
+    soln = await SolnSquareVerifier.new(verifier.address, {
+      from: accounts[0],
+    });
+  });
+  it('can mint a ERC721 if the proof is valid', () => {});
+  it('reverts if proof is not valid', () => {});
+  it('emits SolutionAdded event if a solution is submited', () => {});
+});
